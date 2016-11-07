a41d82c7af1bdc34ade6f7506e3c3c30aa17ad8e
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-11-07 17:19:46 -0600

    Change name of project

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "tinyswift",
+    name: "bookstore",
 	dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
