e3843dbba4f2aea21733d58113cb016c43044f2b
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-11-20 22:15:29 -0600

    Much improved schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,62 +1,74 @@
 DROP TABLE carts;
 DROP TABLE orders;
 DROP TABLE users;
-DROP TABLE book_author;
+DROP TABLE books_authors;
 DROP TABLE books;
 DROP TABLE authors;
 
 
 CREATE TABLE books (
-    book_id     INTEGER PRIMARY KEY,
-    title       VARCHAR(1024),
-    ISBN        VARCHAR(128),
-    year        INTEGER
+    book_id     SERIAL PRIMARY KEY,
+    title       TEXT NOT NULL,
+    ISBN        TEXT NOT NULL,
+    year        INTEGER NOT NULL
 );
+
+CREATE INDEX idx_title_search ON books(title);
 
 CREATE TABLE authors (
-    author_id   INTEGER PRIMARY KEY,
-    name        VARCHAR(512)
+    author_id   SERIAL PRIMARY KEY,
+    name        TEXT NOT NULL 
 );
 
-CREATE TABLE book_author (
-    book_id     INTEGER REFERENCES books,
-    author_id   INTEGER REFERENCES authors 
+CREATE TABLE books_authors (
+    book_id     INTEGER NOT NULL REFERENCES books,
+    author_id   INTEGER NOT NULL REFERENCES authors 
 );
 
 CREATE TABLE users (
-    user_id     INTEGER PRIMARY KEY, 
-    first_name  VARCHAR(512),
-    last_name   VARCHAR(512)
+    user_id     SERIAL PRIMARY KEY, 
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL
 );
 
 CREATE TABLE carts (
-    user_id     INTEGER REFERENCES users,
-    book_id     INTEGER PRIMARY KEY REFERENCES books,
-    quantity    INTEGER 
+    user_id     INTEGER NOT NULL REFERENCES users,
+    book_id     INTEGER NOT NULL REFERENCES books,
+    quantity    INTEGER NOT NULL
 );
 
 CREATE TABLE orders (
-    order_id    INTEGER PRIMARY KEY,
+    order_id    SERIAL PRIMARY KEY,
     user_id     INTEGER REFERENCES users,
     book_id     INTEGER REFERENCES books,
-    quantity    INTEGER  
+    quantity    INTEGER NOT NULL
 );
 
-INSERT INTO books (book_id, title, ISBN, year) VALUES (1, 'A Game of Thrones', '978-0553593716', 2003);
-INSERT INTO books (book_id, title, ISBN, year) VALUES (2, 'A Clash of Kings', '978-0553593716', 2003);
-INSERT INTO books (book_id, title, ISBN, year) VALUES (3, 'A Storm of Swords', '034554398X', 2003);
-INSERT INTO books (book_id, title, ISBN, year) VALUES (4, 'Harry Potter and the Sorcerer''s Stone', '0439708184', 1999);
+INSERT INTO books (book_id, title, ISBN, year) 
+VALUES (1, 'A Game of Thrones', '978-0553593716', 2003),
+       (2, 'A Clash of Kings', '978-0553593716', 2003),
+       (3, 'A Storm of Swords', '034554398X', 2003),
+       (4, 'Harry Potter and the Sorcerer''s Stone', '0439708184', 1999),
+       (5, 'A Dance with Dragons', '0553582011', 2013);
 
-INSERT INTO authors (author_id, name) VALUES (1, 'George R. R. Martin');
-INSERT INTO authors (author_id, name) VALUES (2, 'J.K. Rowling');
 
-INSERT INTO book_author (book_id, author_id) VALUES (1, 1);
-INSERT INTO book_author (book_id, author_id) VALUES (2, 1);
-INSERT INTO book_author (book_id, author_id) VALUES (3, 1);
-INSERT INTO book_author (book_id, author_id) VALUES (4, 2);
+INSERT INTO authors (author_id, name) 
+VALUES (1, 'George R. R. Martin'),
+       (2, 'J.K. Rowling');
 
-INSERT INTO users (user_id, first_name, last_name) VALUES (1, 'Robert', 'Dickerson');
 
-INSERT INTO orders (order_id, user_id, book_id, quantity) VALUES (1, 1, 1, 1);
+INSERT INTO books_authors (book_id, author_id) 
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (4, 2),
+       (5, 1);
 
-INSERT INTO carts (user_id, book_id, quantity) VALUES (1, 4, 2);
+INSERT INTO users (user_id, first_name, last_name) 
+VALUES (1, 'Robert', 'Dickerson');
+
+INSERT INTO orders (order_id, user_id, book_id, quantity) 
+VALUES (1, 1, 1, 1);
+
+INSERT INTO carts (user_id, book_id, quantity) 
+VALUES (1, 4, 2);
